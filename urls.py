3e362cb67fb2fb5f6cd84c0ca3234139f1824abb
seb <sebastian.thomas@u-dox.com>
2013-01-08 09:45:03 +0000

    Added root index view

--- urls.py
+++ urls.py
@@ -1,9 +1,23 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import *
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
 
+from mapsrobot import views
+
 urlpatterns = patterns('',
+    # Example:
+    # (r'^acollabauth/', include('acollabauth.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
     url(r'^marker/', include('marker.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes' : True }),
 )
