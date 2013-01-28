d1ea8155f56e20c1e9124a1617bdb0a89e2f5e98
seb <sebastian.thomas@u-dox.com>
2013-01-28 19:49:39 +0000

    Removed geopostion widget (thanks tho :) ), added simple js lat lng finder instead

--- marker/admin.py
+++ marker/admin.py
@@ -9,6 +9,6 @@ class MarkerAdmin(admin.ModelAdmin):
         from django.conf import settings
         js = ('http://maps.google.com/maps/api/js?sensor=true',
             'https://raw.github.com/HPNeo/gmaps/master/gmaps.js',
-            '../media/js/MarkerAdmin.js')
+            '/media/js/MarkerAdmin.js')
 
 admin.site.register(Marker, MarkerAdmin)
--- media/js/MarkerAdmin.js
+++ media/js/MarkerAdmin.js
@@ -6,31 +6,45 @@ if (jQuery != undefined) {
 (function($) {
 
     $(document).ready(function() {
-        $searchInput = $('<input>', {'type': 'search', 'placeholder': 'Search …', 'id': 'address'});
+        $searchInput = $('<input>', {'type': 'search', 'placeholder': 'Search …', 'id': 'address', 'width': '300px'});
         $map = $('<div>', {'id': 'map', 'height': '300px'});
+        $submit = $('<button>', {'type': 'button', 'text': 'Search', 'id': 'search'});
         $searchRow = $('.field-longitude');
-        $searchInput.appendTo($searchRow);
-        $map.appendTo($searchRow);
+
+        $searchRow.append($searchInput, $submit, $map);
+
         map = new GMaps({
             div: '#map',
-            lat: -12.043333,
-            lng: -77.028333
+            lat: 51.48425033829694,
+            lng: -0.10845702578126781,
+            zoom: 12,
         });
-        $('#address').keyup(function (e) {
-            e.preventDefault();
+
+        function getlatLng(){
             GMaps.geocode({
                 address: $('#address').val().trim(),
                 callback: function(results, status){
                     if(status=='OK'){
                         var latlng = results[0].geometry.location;
                         map.setCenter(latlng.lat(), latlng.lng());
+                        map.setZoom(16);
                         map.addMarker({
                             lat: latlng.lat(),
                             lng: latlng.lng()
                         });
+                        $('#id_latitude').val(latlng.lat())
+                        $('#id_longitude').val(latlng.lng())
                     }
                 }
             });
+        }
+
+        $submit.click(getlatLng);
+        $searchInput.bind('keydown', function(e) {
+            if (e.keyCode == 13) {
+                e.preventDefault();
+                getlatLng();
+            }
         });
     });
 })(django.jQuery);
